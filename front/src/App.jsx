65a549dd65c0dcedec06479d290546e0755b860f
wookgom <kjw3957@gmail.com>
2024-11-28 22:23:48 +0900

    [feat] App.jx에 게시판 이동 루트 추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
+import DevBoard from './pages/DevBoard/DevBoard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dev-board" element={<DevBoard />} />
       </Routes>
     </Router>
   )
