bc95d4247793219c09db895efa87254007864518
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2024-12-02 07:14:18 +0900

    [feat] : Add DevConf page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
 import DevBoard from './pages/DevBoard/DevBoard';
 import PostDetail from './pages/DevBoard/PostDetail';
+import DevConf from './pages/DevConf/DevConf';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dev-board" element={<DevBoard />} />
         <Route path="/dev-board/:postId" element={<PostDetail />} />
+        <Route path="/dev-conf" element={<DevConf />} />
       </Routes>
     </Router>
   )
